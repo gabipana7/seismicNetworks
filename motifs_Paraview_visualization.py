fc018750ec0d288e000ff73384e2c99f02621710
gabipana7 <gabi_pn_ti@yahoo.com>
2022-04-21 15:57:40 +0300

    paraview viz added

---
+++ motifs_Paraview_visualization.py
@@ -0,0 +1,131 @@
+import vtk
+import networkx as nx
+
+import os 
+
+import ast
+import numpy as np
+
+from writeNodesEdges import writeObjects
+from writeMotifs import writeObjectsMotifs
+
+
+# ---------------------------SETUP AND COLLECTION OF QUAKES------------------------------------#
+# Which region do you want to analyze ?
+region = input('Input region : vrancea / romania / california / italy / japan : ')
+
+# The side of the cubes that you split the region in 
+side = int(input('Input side of the cube split in km 5 / 10 / 20 - recommended = 5 : '))
+
+# Select desired magnitude threshold
+mag = int(input('Select desired magnitude threshold - recommended = 4 : '))
+
+G = nx.read_gexf(f'./results/{region}/networks/view/network{region}_{side}km_{mag}mag.gexf')
+
+
+motif = input('Input motif: Triangles (for areas) / Tetrahedrons (for volumes) : ')
+
+
+#---------------------------------------MOTIFS-----------------------------------------#
+
+# Use readlines() to open the 
+fileMotif = open(f'./results/{region}/networks/motifs/motifs{motif}_network{region}_{side}km_{mag}mag.txt', 'r')
+linesMotif = fileMotif.readlines()
+
+# Properly evaluate the Lines to get the Lists
+motifNodes=[]
+for item in linesMotif:
+    motifNodes.append(ast.literal_eval(item))
+
+
+if motif == 'Triangles':    
+    # Graph containing triangles only
+    H = nx.Graph()
+    for item in motifNodes:
+        H.add_edge(int(item[0]),int(item[1]))
+        H.add_edge(int(item[1]),int(item[2]))
+        H.add_edge(int(item[0]),int(item[2]))
+
+else:
+    # Graph containing squares only
+    H = nx.Graph()
+    for item in motifNodes:
+        H.add_edge(int(item[0]),int(item[1]))
+        H.add_edge(int(item[1]),int(item[2]))
+        H.add_edge(int(item[2]),int(item[3]))
+        H.add_edge(int(item[0]),int(item[3]))
+
+    
+# Set the triangle attribute = 0 to each edge
+nx.set_edge_attributes(G, 0, name=motif)
+
+G = nx.convert_node_labels_to_integers(G,first_label=1)
+
+# Iterate through our triangle only network edges
+for (u,v) in H.edges():
+    # Assign to original network edges the attribute triangle = 1
+    G[u][v][motif] = 1
+
+
+# Get attributes that go into VTK function
+
+# X Y Z coords
+lat =[]
+long = []
+depth =[]
+for n in G.nodes():
+    lat.append(int(G.nodes[n]['quake_xLatitude']))
+    long.append(int(G.nodes[n]['quake_yLongitude']))
+    depth.append(int(G.nodes[n]['quake_zDepth']))
+
+minLat = min(lat)
+maxLat = max(lat)
+minLong = min(long)
+maxLong = max(long)
+minDepth = min(depth)
+maxDepth = max(depth)
+maxDimension = max(maxLat,maxLong,maxDepth)
+
+
+coords=[]
+for n in G.nodes():
+    coords.append([ np.float32(round((int(G.nodes[n]['quake_xLatitude']) - minLat)*(maxLat/maxDimension)/(maxLat-minLat),3)),
+                    np.float32(round((int(G.nodes[n]['quake_yLongitude']) - minLong)*(maxLong/maxDimension)/(maxLong-minLong),3)),
+                    np.float32(round((int(G.nodes[n]['quake_zDepth']) - minDepth)*(maxDepth/maxDimension)/(maxDepth-minDepth),3))])
+  
+# Degree of nodes edges    
+degree = [d for n, d in G.degree()]
+
+# Weight of edges
+weights = []
+for (i,j) in G.edges():
+    weights.append(G[i][j]['weight'])
+    
+# Triangle quality of edges
+motifs = []
+for (i,j) in G.edges():
+    motifs.append(G[i][j][motif])
+
+
+
+if not os.path.exists(f'./results/{region}/networksview'):
+    os.makedirs(f'./results/{region}/networksview')
+os.chdir(f'./results/{region}/networksview')
+
+
+
+# Write the VTK file that goes in Paraview
+# The network
+writeObjects(nodeCoords=coords,
+             edges=G.edges(),
+             scalar=degree, name='degree',
+             scalar2=weights, name2='weight',
+             escalar2=motifs, ename2=motif,
+#             nodeLabel=nodeLabel,
+             fileout=f'network{region}_{side}km_{mag}mag_{motif}')
+
+# Only the motifs
+writeObjectsMotifs(nodeCoords=coords,
+             motifCoords=motifNodes,
+             fileout=f'network{region}_{side}km_{mag}mag_{motif}Only')
+
---
+++ writeNodesEdges.py
@@ -0,0 +1,146 @@
+import vtk
+
+def writeObjects(nodeCoords,
+                 motifCoords={},
+                 edges = [],
+                 scalar = [], name = '', power = 1,
+                 scalar2 = [], name2 = '', power2 = 1,
+                 escalar = [], ename = '', epower = 1,
+                 escalar2 = [], ename2 = '', epower2 =1,
+                 nodeLabel = [],
+                 method = 'vtkPolyData',
+                 fileout = 'test'):
+    """
+    Store points and/or graphs as vtkPolyData or vtkUnstructuredGrid.
+    Required argument:
+    - nodeCoords is a list of node coordinates in the format [x,y,z]
+    Optional arguments:
+    - edges is a list of edges in the format [nodeID1,nodeID2]
+    - scalar/scalar2 is the list of scalars for each node
+    - name/name2 is the scalar's name
+    - power/power2 = 1 for r~scalars, 0.333 for V~scalars
+    - nodeLabel is a list of node labels
+    - method = 'vtkPolyData' or 'vtkUnstructuredGrid'
+    - fileout is the output file name (will be given .vtp or .vtu extension)
+    """
+
+    # INITIALIZE THE NODES 
+    points = vtk.vtkPoints()
+    for node in nodeCoords:
+        points.InsertNextPoint(node)   
+
+    if motifCoords:
+        # INITIALIZE TRIANGLES
+        if len(motifCoords[0]) == 3:
+            triangles = vtk.vtkCellArray()
+
+            # Initialize the points for the triangle
+
+            for motif in motifCoords:
+                triangle = vtk.vtkTriangle()
+                
+                keys=[]
+                for nodeIdx in motif.keys():
+                    keys.append(nodeIdx)
+
+                    
+                triangle.GetPointIds().SetId(0,keys[0]-1)
+                triangle.GetPointIds().SetId(1,keys[1]-1)
+                triangle.GetPointIds().SetId(2,keys[2]-1)
+            
+                # Append the newly created triangle to the triangles
+                triangles.InsertNextCell(triangle)  
+
+
+    # INITIALIZE THE EDGES
+    if edges:
+        line = vtk.vtkCellArray()
+        line.Allocate(len(edges))
+        for edge in edges:
+            line.InsertNextCell(2)
+            line.InsertCellPoint(edge[0])
+            line.InsertCellPoint(edge[1])   # line from point edge[0] to point edge[1]
+
+    if scalar:
+        attribute = vtk.vtkFloatArray()
+        attribute.SetNumberOfComponents(1)
+        attribute.SetName(name)
+        attribute.SetNumberOfTuples(len(scalar))
+        for i, j in enumerate(scalar):   # i becomes 0,1,2,..., and j runs through scalars
+            attribute.SetValue(i,j**power)
+
+    if scalar2:
+        attribute2 = vtk.vtkFloatArray()
+        attribute2.SetNumberOfComponents(1)
+        attribute2.SetName(name2)
+        attribute2.SetNumberOfTuples(len(scalar2))
+        for i, j in enumerate(scalar2):   # i becomes 0,1,2,..., and j runs through scalar2
+            attribute2.SetValue(i,j**power2)
+            
+    if escalar:
+        eattribute = vtk.vtkFloatArray()
+        eattribute.SetNumberOfComponents(1)
+        eattribute.SetName(ename)
+        eattribute.SetNumberOfTuples(len(edges))
+        for i, j in enumerate(escalar):   # i becomes 0,1,2,..., and j runs through scalar2
+            eattribute.SetValue(i,j**epower)
+
+    if escalar2:
+        eattribute2 = vtk.vtkFloatArray()
+        eattribute2.SetNumberOfComponents(1)
+        eattribute2.SetName(ename2)
+        eattribute2.SetNumberOfTuples(len(edges))
+        for i, j in enumerate(escalar2):  # i becomes 0,1,2,..., and j runs through escalars
+            eattribute2.SetValue(i, j**epower2)
+
+
+    if nodeLabel:
+        label = vtk.vtkFloatArray()
+        label.SetName('tag')
+        label.SetNumberOfValues(len(nodeLabel))
+        for i, j in enumerate(nodeLabel):   # i becomes 0,1,2,..., and j runs through scalar
+            label.SetValue(i,j)
+
+    if method == 'vtkPolyData':
+        polydata = vtk.vtkPolyData()
+        polydata.SetPoints(points)
+
+        if motifCoords:
+            polydata.SetPolys(triangles)
+
+        if edges:
+            polydata.SetLines(line)
+        if scalar:
+            polydata.GetPointData().AddArray(attribute)
+        if scalar2:
+            polydata.GetPointData().AddArray(attribute2)
+        if escalar:
+            polydata.GetCellData().AddArray(eattribute)
+        if escalar2:
+            polydata.GetCellData().AddArray(eattribute2)
+        if nodeLabel:
+            polydata.GetPointData().AddArray(label)
+        writer = vtk.vtkXMLPolyDataWriter()
+        writer.SetFileName(fileout+'.vtp')
+        writer.SetInputData(polydata)
+        writer.Write()
+    elif method == 'vtkUnstructuredGrid':
+        # caution: ParaView's Tube filter does not work on vtkUnstructuredGrid
+        grid = vtk.vtkUnstructuredGrid()
+        grid.SetPoints(points)
+        if edges:
+            grid.SetCells(vtk.VTK_LINE, line)
+        if scalar:
+            grid.GetPointData().AddArray(attribute)
+        if scalar2:
+            grid.GetPointData().AddArray(attribute2)
+        if escalar:
+            grid.GetCellData().AddArray(eattribute)
+        if escalar2:
+            grid.GetCellData().AddArray(eattribute2)
+        if nodeLabel:
+            grid.GetPointData().AddArray(label)
+        writer = vtk.vtkXMLUnstructuredGridWriter()
+        writer.SetFileName(fileout+'.vtu')
+        writer.SetInputData(grid)
+        writer.Write()
